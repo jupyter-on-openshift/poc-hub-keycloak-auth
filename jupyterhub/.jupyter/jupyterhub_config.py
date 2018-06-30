14a5496b5004e0461fcbacbdeb0ae22fd5844a89
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-07-01 08:13:27 +1000

    Separate volume for each user.

--- jupyterhub/.jupyter/jupyterhub_config.py
+++ jupyterhub/.jupyter/jupyterhub_config.py
@@ -4,8 +4,10 @@ import escapism
 
 # Enable JupyterLab interface if enabled.
 
+c.Spawner.environment = {}
+
 if os.environ.get('JUPYTERHUB_ENABLE_LAB', 'false').lower() in ['true', 'yes', 'y', '1']:
-    c.Spawner.environment = dict(JUPYTER_ENABLE_LAB='true')
+    c.Spawner.environment.update(dict(JUPYTER_ENABLE_LAB='true'))
 
 # Setup location for customised template files.
 
@@ -79,86 +81,31 @@ if os.path.exists('/opt/app-root/configs/user_whitelist.txt'):
         if content:
             c.Authenticator.whitelist = set(content.split())
 
-# Provide persistent storage for users notebooks. We share one
-# persistent volume for all users, mounting just their subdirectory into
-# their pod. The persistent volume type needs to be ReadWriteMany so it
-# can be mounted on multiple nodes as can't control where pods for a
-# user may land. Because it is a shared volume, there are no quota
-# restrictions which prevent a specific user filling up the entire
-# persistent volume.
+# Provide persistent storage for users notebooks.
 
-c.KubeSpawner.user_storage_pvc_ensure = False
-c.KubeSpawner.pvc_name_template = '%s-notebooks' % c.KubeSpawner.hub_connect_ip
+c.KubeSpawner.user_storage_pvc_ensure = True
+
+c.KubeSpawner.pvc_name_template = '%s-nb-{username}' % c.KubeSpawner.hub_connect_ip
+c.KubeSpawner.user_storage_capacity = '1Gi'
 
 c.KubeSpawner.volumes = [
     {
-        'name': 'notebooks',
+        'name': 'data',
         'persistentVolumeClaim': {
             'claimName': c.KubeSpawner.pvc_name_template
         }
     }
 ]
 
-volume_mounts_user = [
+c.KubeSpawner.volume_mounts = [
     {
-        'name': 'notebooks',
-        'mountPath': '/opt/app-root/src',
-        'subPath': '{username}'
+        'name': 'data',
+        'mountPath': '/opt/app-root/src'
     }
 ]
 
-volume_mounts_admin = [
-    {
-        'name': 'notebooks',
-        'mountPath': '/opt/app-root/src/users'
-    }
-]
-
-def interpolate_properties(spawner, template):
-    safe_chars = set(string.ascii_lowercase + string.digits)
-    username = escapism.escape(spawner.user.name, safe=safe_chars,
-            escape_char='-').lower()
-
-    return template.format(
-        userid=spawner.user.id,
-        username=username
-        )
-
-def expand_strings(spawner, src):
-    if isinstance(src, list):
-        return [expand_strings(spawner, i) for i in src]
-    elif isinstance(src, dict):
-        return {k: expand_strings(spawner, v) for k, v in src.items()}
-    elif isinstance(src, str):
-        return interpolate_properties(spawner, src)
-    else:
-        return src
-
-def modify_pod_hook(spawner, pod):
-    if spawner.user.admin:
-        volume_mounts = volume_mounts_admin
-        workspace = interpolate_properties(spawner, 'users/{username}/workspace')
-    else:
-        volume_mounts = volume_mounts_user
-        workspace = 'workspace'
-
-    try:
-        os.mkdir(interpolate_properties(spawner, '/opt/app-root/notebooks/{username}'))
-
-    except IOError:
-        pass
-
-    pod.spec.containers[0].env.append(dict(name='JUPYTER_MASTER_FILES',
-            value='/opt/app-root/master'))
-    pod.spec.containers[0].env.append(dict(name='JUPYTER_WORKSPACE_NAME',
-            value=workspace))
-
-    pod.spec.containers[0].volume_mounts.extend(
-            expand_strings(spawner, volume_mounts))
-
-    return pod
-
-c.KubeSpawner.modify_pod_hook = modify_pod_hook
+c.Spawner.environment.update(dict(JUPYTER_MASTER_FILES='/opt/app-root/master',
+    JUPYTER_WORKSPACE_NAME='/opt/app-root/src/workspace'))
 
 # Setup culling of idle notebooks if timeout parameter is supplied.
 
